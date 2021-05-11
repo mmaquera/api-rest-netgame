01db116b5214f3fd6fc4aefdb980c7b820e2d6b9
Marco Maquera <marco.arka@gmail.com>
2021-05-10 22:25:40 -0500

    update package.json

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/netgame',
+  db: process.env.MONGODB_URI || 'mongodb+srv://demo:demo@cluster0.aw98t.mongodb.net/netgame?retryWrites=true&w=majority',
   SECRET_TOKEN: 'mySecretToken'
 }
